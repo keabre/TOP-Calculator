846a1db296bc1044571d0d456b0752284a9de9d8
keabre <104922102+keabre@users.noreply.github.com>
2024-06-05 23:51:02 +0100

    Link JS file and store DOM elements in variables

---
+++ app.js
@@ -0,0 +1,11 @@
+// Gather DOM elements
+
+const digitButtons = document.querySelectorAll('.digit')
+
+const operatorButtons = document.querySelectorAll('.operator')
+
+const clearButton = document.getElementById('clear')
+
+const equalsButton = document.getElementById('equals')
+
+const calculatorDisplay = document.getElementById('calc-display')
